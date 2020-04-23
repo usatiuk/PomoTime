6cac3d9cc8a8e5e0c03d60e5ca4f26970c687433
Stepan Usatiuk <stepanusatiuk@gmail.com>
2020-04-23 21:52:45 +0300

    everything mostly works

--- Pomotimer/MainPage.xaml.cs
+++ Pomotimer/MainPage.xaml.cs
@@ -8,11 +8,13 @@ using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using controls = Microsoft.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
 
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -24,15 +26,138 @@ namespace Pomotimer
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DispatcherTimer dispatcherTimer;
+        public RunningState runningState = new RunningState();
+
+        private int RestMinutes { get; set; }
+        private int WorkMinutes { get; set; }
+
         public MainPage()
         {
             this.InitializeComponent();
 
+            // Some maigc numbers
             Size defaultSize = new Size(500, 250);
 
             ApplicationView.PreferredLaunchViewSize = defaultSize;
             ApplicationView.GetForCurrentView().SetPreferredMinSize(defaultSize);
             ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+
+            Application.Current.Suspending += OnSuspending;
+            this.Loaded += MainPageLoaded;
+
+            ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            Windows.Storage.ApplicationDataCompositeValue minutes = (ApplicationDataCompositeValue)roamingSettings.Values["Minutes"];
+            if (minutes != null)
+            {
+                WorkMinutes = (int)minutes["WorkMinutes"];
+                RestMinutes = (int)minutes["RestMinutes"];
+            }
+            else
+            {
+                // Some maigc defualt numbers
+                WorkMinutes = 40;
+                RestMinutes = 5;
+            }
+            runningState.MinutesLeft = WorkMinutes;
+            runningState.SecondsLeft = 0;
+
+        }
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            if (runningState.SecondsLeft == 0)
+            {
+                runningState.SecondsLeft = 59;
+                if (runningState.MinutesLeft == 0)
+                {
+                    if (runningState.OnRest)
+                    {
+                        runningState.OnRest = false;
+                        runningState.MinutesLeft = WorkMinutes;
+                        runningState.SecondsLeft = 0;
+                    }
+                    else
+                    {
+                        runningState.OnRest = true;
+                        runningState.MinutesLeft = RestMinutes;
+                        runningState.SecondsLeft = 0;
+                    }
+                }
+                else
+                {
+                    runningState.MinutesLeft--;
+                }
+            }
+            else
+            {
+                runningState.SecondsLeft--;
+            }
+        }
+
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            dispatcherTimer.Start();
+            runningState.IsRunning = true;
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            dispatcherTimer.Stop();
+            runningState.IsRunning = false;
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            dispatcherTimer.Stop();
+            runningState.IsRunning = false;
+
+            runningState.OnRest = false;
+            runningState.MinutesLeft = WorkMinutes;
+            runningState.SecondsLeft = 0;
+        }
+
+        private void Plus1Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            runningState.MinutesLeft += 1;
+        }
+
+        private void Plus5Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            runningState.MinutesLeft += 5;
+        }
+
+        private void Plus10Button_Click(object sender, RoutedEventArgs e)
+        {
+            AppBarButton b = sender as AppBarButton;
+            runningState.MinutesLeft += 10;
+        }
+        private void OnSuspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            ApplicationDataContainer roamingSettings = Windows.Storage.ApplicationData.Current.RoamingSettings;
+            Windows.Storage.ApplicationDataCompositeValue minutes = new Windows.Storage.ApplicationDataCompositeValue();
+            minutes["WorkMinutes"] = WorkMinutes;
+            minutes["RestMinutes"] = RestMinutes;
+            roamingSettings.Values["Minutes"] = minutes;
+            deferral.Complete();
+        }
+
+        private void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimerSetup();
         }
     }
 }
---
+++ Pomotimer/RunningState.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomotimer
+{
+    public class RunningState : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private bool _is_running;
+        public bool IsRunning
+        {
+            get { return _is_running; }
+            set
+            {
+                _is_running = value;
+                NotifyPropertyChanged("IsRunning");
+            }
+
+        }
+
+        public int _minutes_left;
+        public int MinutesLeft
+        {
+            get { return _minutes_left; }
+            set
+            {
+                _minutes_left = value;
+                NotifyPropertyChanged("MinutesLeft");
+            }
+
+        }
+
+        public int _seconds_left;
+        public int SecondsLeft
+        {
+            get { return _seconds_left; }
+            set
+            {
+                _seconds_left = value;
+                NotifyPropertyChanged("SecondsLeft");
+            }
+
+        }
+
+        public bool _on_rest;
+
+        public bool OnRest
+        {
+            get { return _on_rest; }
+            set
+            {
+                _on_rest = value;
+                NotifyPropertyChanged("OnRest");
+            }
+
+        }
+    }
+}
